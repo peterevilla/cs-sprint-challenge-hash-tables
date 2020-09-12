39164c759362b759f5c413618de362f3f43dd76b
Pedro Revilla <peterevilla@gmail.com>
2020-09-11 21:49:01 -0500

    some stretch

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,14 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    
+    cache = {}
+    x = None
+    for i in range(0, length):
+        cache[weights[i]] = i 
+  
+    for j in range(0, length):
+        x = limit - weights[j]
+        if x in cache:
+          return [cache[x], j]
+            
+    
     return None
